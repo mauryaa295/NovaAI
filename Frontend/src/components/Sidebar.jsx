ab8e776960cda22ae70ba248d1ea9e607c3b8103
mauryaa295 <mauryaa295@gmail.com>
2026-07-20 20:09:51 +0530

    Update NovaAI logo

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -83,7 +83,7 @@ const Sidebar = () => {
     <section className="sidebar">
       {/* new chat button  */}
       <button onClick={createNewChat}>
-        <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo" />
+        <img src="/blacklogo.png" alt="gpt-logo" className="logo" />
 
         <span>
           <i className="fa-solid fa-pen-to-square"></i>
